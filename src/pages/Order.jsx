832e49354efca5a5afa643b21818f6eb6911833a
HN721 <119390246+HN721@users.noreply.github.com>
2024-12-02 13:45:32 +0700

    add-movie

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -23,12 +23,12 @@ export default function Order() {
   return (
     <>
       <Navbar />
-      <div className="min-h-screen bg-gray-100 px-4 pt-6">
+      <div className="min-h-screen  px-4 pt-6">
         <form
           onSubmit={handleSubmit}
-          className="bg-white p-4 rounded shadow-md w-full max-w-sm mx-auto"
+          className="bg-white p-6 rounded shadow-md w-full"
         >
-          <h2 className="text-2xl font-bold mb-4 text-center">Input Order</h2>
+          <h2 className="text-2xl font-bold mb-6 text-center">Input Order</h2>
 
           {/* Nama */}
           <div className="mb-4">
@@ -112,7 +112,7 @@ export default function Order() {
           {/* Submit Button */}
           <button
             type="submit"
-            className="w-full bg-blue-500 text-white py-2 px-4 rounded-lg shadow hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+            className="w-full bg-blue-500 text-white py-3 rounded-lg shadow hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
           >
             Submit
           </button>
