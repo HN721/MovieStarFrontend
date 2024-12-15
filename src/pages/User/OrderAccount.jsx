f49b18651a46530f0881e10a61ad7b8e52e02ac0
HN721 <119390246+HN721@users.noreply.github.com>
2024-12-15 21:29:53 +0700

    addreadme

--- src/pages/User/OrderAccount.jsx
+++ src/pages/User/OrderAccount.jsx
@@ -1,15 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { getOneOrder } from "../../services/Order";
 import Navbar from "../../component/Navbar";
 import Footer from "../Fotter";
 
 export default function OrderAccount() {
+  const [nama, setNama] = useState("");
+  const [jadwal, setJadwal] = useState("");
   const user = useSelector((state) => state.auth.user.id);
   useEffect(() => {
     const fetchOrder = async () => {
       try {
         const res = await getOneOrder(user);
+        // setNama(res.user.name)
+        setJadwal(res.jadwal.bioskop.nama);
         console.log(res);
       } catch (e) {
         return e;
@@ -20,10 +24,10 @@ export default function OrderAccount() {
   return (
     <>
       <Navbar />
-      <div>
+      <div className="px-6">
         <h1 className="text-2xl font-arimo text-center mt-5">Ticket Saya</h1>
-        <div>
-          <h1></h1>
+        <div className="bg-slate-400 w-full">
+          <h1>{jadwal}</h1>
           <p></p>
         </div>
       </div>
