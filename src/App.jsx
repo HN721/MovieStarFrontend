e2bfe6ff0853a179bc4f31109577cadf4d2bc512
HN721 <119390246+HN721@users.noreply.github.com>
2024-12-19 13:05:00 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import {
   Navigate,
 } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; // import QueryClient and QueryClientProvider
 import AdminRoutes from "./pages/admin/AdminRoutes";
 import Login from "./component/Login";
 import Register from "./component/Register";
@@ -19,75 +19,78 @@ import Account from "./component/User/Account";
 import Ticket from "./pages/User/Ticket";
 import AuthRoute from "./component/auth/AuthRoute";
 import { getToken } from "./utils/getToken";
+import AuthorizationRoute from "./component/auth/AuthorizationRoute";
 import OrderAccount from "./pages/User/OrderAccount";
 
-const queryClient = new QueryClient();
+// Create an instance of QueryClient
 
 const App = () => {
   const token = getToken();
   return (
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/detail/:id"
-            element={
-              <AuthRoute>
-                <DetailMovie />
-              </AuthRoute>
-            }
-          />
-          <Route
-            path="/detail/jadwal"
-            element={
-              <AuthRoute>
-                <JadwalList />
-              </AuthRoute>
-            }
-          />
-          <Route
-            path="account/order"
-            element={
-              <AuthRoute>
-                <OrderAccount />
-              </AuthRoute>
-            }
-          />
-          <Route path="/account" element={<Account />} />
-          <Route
-            path="/ticket/:id"
-            element={
-              <AuthRoute>
-                <Seat />
-              </AuthRoute>
-            }
-          />
-          <Route
-            path="/order/:id"
-            element={
-              <AuthRoute>
-                <Order />
-              </AuthRoute>
-            }
-          />
-          <Route path="/account/ticket/:id" element={<Ticket />} />
-          <Route
-            path="/admin/*"
-            element={
-              <AuthRoute>
-                <AdminRoutes />
-              </AuthRoute>
-            }
-          />
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="/register"
-            element={token ? <AdminRoutes /> : <Register />}
-          />
-        </Routes>
-      </Router>
-    </QueryClientProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/detail/:id"
+          element={
+            <AuthRoute>
+              <DetailMovie />
+            </AuthRoute>
+          }
+        />
+        <Route
+          path="/detail/jadwal"
+          element={
+            <AuthRoute>
+              <JadwalList />
+            </AuthRoute>
+          }
+        />
+        <Route
+          path="account/order"
+          element={
+            <AuthRoute>
+              <OrderAccount />
+            </AuthRoute>
+          }
+        />
+        <Route path="/account" element={<Account />} />
+        <Route
+          path="/ticket/:id"
+          element={
+            <AuthRoute>
+              <Seat />
+            </AuthRoute>
+          }
+        />
+        <Route
+          path="/order/:id"
+          element={
+            <AuthRoute>
+              <Order />
+            </AuthRoute>
+          }
+        />
+        <Route path="/account/ticket/:id" element={<Ticket />} />
+
+        <Route
+          path="/admin/*"
+          element={
+            <AuthRoute>
+              <AdminRoutes />
+              {/* <AuthorizationRoute>
+                 
+                </AuthorizationRoute> */}
+            </AuthRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/register"
+          element={token ? <AdminRoutes /> : <Register />}
+        />
+      </Routes>
+    </Router>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,15 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store/store.js";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </Provider>
   </StrictMode>
 );
